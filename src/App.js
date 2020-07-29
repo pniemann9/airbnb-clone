5646ca89f8b2c92499081958e0f00e135006dbf3
Philippe Niemann <pniemann@orange.fr>
2020-07-29 18:57:54 +0200

    Add a search bar, marker on a map...

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import GoogleMapReact from 'google-map-react';
 // import logo from './logo.svg';
 import './App.css';
 import Flat from './components/flat';
+import Marker from './components/marker';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flats: []
+      flats: [],
+      allFlats: [],
+      selectFlat: null,
+      search: ""
     };
   }
   componentDidMount(){
@@ -17,31 +21,65 @@ class App extends Component {
       .then(response => response.json())
       .then((data) => {
         this.setState({
-            flats: data
+            flats: data,
+            allFlats: data
         });
       })
   }
+  selectFlat = (flat) => {
+    this.setState({
+      selectFlat: flat
+    })
+  }
+  handleSearch = (event) => {
+    this.setState({
+      search: event.target.value,
+      flats: this.state.allFlats.filter((flat) => new RegExp(event.target.value, "i").exec(flat.name))
+    });
+  }
   render() {
-    const center = {
+    let center = {
       lat: 48.8566,
       lng: 2.3522
+    }
+    if (this.state.selectedFlat) {
+      center = {
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng
+      }
     }
     return (
       <div className="app">
         <div className="main">
           <div className="search">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.state.search}
+              onChange={this.handleSearch} />
           </div>
           <div className="flats">
             {this.state.flats.map((flat) => {
-              return <Flat flat={flat} />
+              return <Flat
+                key={flat.name}
+                flat={flat}
+                selectFlat={this.selectFlat} />
             })}
           </div>
         </div>
         <div className="map">
           <GoogleMapReact
             center={center}
-            zoom={11}
+            zoom={14}
           >
+            {this.state.flats.map((flat) => {
+              return <Marker
+                key={flat.name}
+                lat={flat.lat}
+                lng={flat.lng}
+                text={flat.price}
+                selected={flat === this.state.selectedFlat } />
+            })}
           </GoogleMapReact>
         </div>
       </div>
--- src/components/flat.js
+++ src/components/flat.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./flat.css";
 
 class Flat extends React.Component {
+  handleClick = () => {
+    // Call the parent method selectFlat
+    this.props.selectFlat(this.props.flat);
+  }
   render () {
 
     const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
@@ -12,7 +16,7 @@ class Flat extends React.Component {
     };
 
     return (
-      <div className="flat">
+      <div className="flat" onClick={this.handleClick}>
         <div className="flat-picture" style={style}></div>
         <div className="flat-title"></div>
           {title}
